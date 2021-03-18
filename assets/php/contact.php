0697ed992783c3d159d6e6cc3b4cbcfbe501b18c
utkarshbhardwaj01 <67866657+utkarshbhardwaj01@users.noreply.github.com>
2021-03-18 14:04:37 +0530

    fixed form

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,48 +1,11 @@
 <?php
-
-// ENTER YOUR EMAIL
-$emailTo = "utkarshbhardwajmail@gmail.com";
-
-// ENTER IDENTIFIER
-$emailIdentifier =  "Message sent via contact form, from " . $_SERVER["Personal Website"];
-
-
-if($_POST) {
-
-    $name = addslashes(trim($_POST["name"]));
-    $clientEmail = addslashes(trim($_POST["email"]));
-    $message = addslashes(trim($_POST["message"]));
-	$fhp_input = addslashes(trim($_POST["company"]));
-
-    $array = array("nameMessage" => "", "emailMessage" => "", "messageMessage" => "","succesMessage" => "");
-
-    if($name == "") {
-    	$array["nameMessage"] = "x";
-    }
-
-    if(!filter_var($clientEmail, FILTER_VALIDATE_EMAIL)) {
-        $array["emailMessage"] = "x";
-    }
-
-    if($message == "") {
-        $array["messageMessage"] = "x";
-    }
-
-    if($name != "" && filter_var($clientEmail, FILTER_VALIDATE_EMAIL) && $message != "" && $fhp_input == "") {
-
-		$array["succesMessage"] = "";
-
-		$headers  = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= "From: " . $name . " <" . $clientEmail .">\r\n";
-		$headers .= "Reply-To: " . $clientEmail;
-
-		mail($emailTo, $emailIdentifier, $message, $headers);
-
-    }
-
-    echo json_encode($array);
-
-}
-
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "utkarshbhardwajmail@gmail.com";
+$subject = "Contact Form Submission";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
